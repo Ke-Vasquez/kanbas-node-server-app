aa7757a531c30c3368755a9d1312c402865cad06
Ke-Vasquez <kevinevasquez6@gmail.com>
2023-12-04 20:38:52 -0500

    please work

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ import session from "express-session";
 import "dotenv/config";
 
 
-console.log(process.env.DB_CONNECTION_STRING);
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+console.log(CONNECTION_STRING);
 
 const app = express();   
 moongose.connect(CONNECTION_STRING);
@@ -24,18 +23,19 @@ app.use(
     })
 );
 const sessionOptions = {
-    secret: "any string",
-    resave: false,
-    saveUninitialized: false,
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
   };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-    };
-  }
-  app.use(session(sessionOptions));
+}
+app.use(session(sessionOptions));
+
   
 app.use(express.json());
 ModuleRoutes(app);
